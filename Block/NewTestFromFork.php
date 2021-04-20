65acbc931fc4d697b3082a9ad2312e2ab66303b9
emanuelegian <82643587+emanuelegian@users.noreply.github.com>
2021-04-20 09:34:04 +0200

    Create NewTestFromFork.php

---
+++ Block/NewTestFromFork.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EmanueleGian\CsTest\Block;
+
+class NewTestFromFork
+{
+    public function MyUglyMethod2()
+    {
+        return 'This is a looong phrase2. This is a looong phrase. This is a looong phrase. This is a looong phrase. This is a looong phrase. This is a looong phrase. This is a looong phrase. ';
+    }
+    
+    
+}
+
