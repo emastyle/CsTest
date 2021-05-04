90eb4f048568928fd91c6b8409bdd926755cd584
Emanuele Gian <emanuele.gian@snow.dog>
2021-05-04 15:31:39 +0200

    Test CS Again and again

---
+++ Block/Test2.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EmanueleGian\CsTest\Block;
+
+class Test
+{
+    public function MyUglyMethodTest($test)
+    {
+        return 'This is a looong phrase. This is a looong phrase. This is a looong phrase. This is a looong phrase. This is a looong phrase. This is a looong phrase. This is a looong phrase. ';
+    }
+
+
+}
+
+
+
