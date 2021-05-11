ea6083b2ff3847bc18f364c884ad2b3b1d3c6749
emanuelegian <82643587+emanuelegian@users.noreply.github.com>
2021-05-11 17:35:36 +0200

    Create Test12.php

---
+++ Block/Test12.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EmanueleGian\CsTest\Block;
+
+class Test12
+{
+    public function MyUglyMethodTest($test12)
+    {
+        return 'This is a looong phrase. This is a looong phrase. This is a looong phrase. This is a looong phrase. This is a looong phrase. This is a looong phrase. This is a looong phrase. ';
+    }
+
+
+}
+
+
+
