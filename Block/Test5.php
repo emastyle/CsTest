c338d28baaf341d01352e8d1d1a670f5f7efc2d6
emanuelegian <82643587+emanuelegian@users.noreply.github.com>
2021-05-11 10:07:13 +0200

    Create Test5.php

---
+++ Block/Test5.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EmanueleGian\CsTest\Block;
+
+class Test5
+{
+    public function MyUglyMethodTest($test5)
+    {
+        return 'This is a looong phrase. This is a looong phrase. This is a looong phrase. This is a looong phrase. This is a looong phrase. This is a looong phrase. This is a looong phrase. ';
+    }
+
+
+}
+
+
+
