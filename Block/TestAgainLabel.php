5609089413716d5eb84d6e1499af26c853fbe61a
Emanuele Gian <emanuelegian@gmail.com>
2021-05-13 14:59:26 +0200

    Create TestAgainLabel.php

---
+++ Block/TestAgainLabel.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EmanueleGian\CsTest\Block;
+
+class TestAgainLabel
+{
+    public function MyUglyMethodTest($test2)
+    {
+        return 'This is a looong phrase. This is a looong phrase. This is a looong phrase. This is a looong phrase. This is a looong phrase. This is a looong phrase. This is a looong phrase. ';
+    }
+
+
+}
+
+
+
