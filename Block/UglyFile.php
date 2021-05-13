fb1b9e6355186e735d68ab62793d04b0dcf61f7e
emanuelegian <82643587+emanuelegian@users.noreply.github.com>
2021-05-13 10:59:19 +0200

    Create UglyFile.php

---
+++ Block/UglyFile.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EmanueleGian\CsTest\Block;
+
+class UglyFile
+{
+    public function MyUglyMethod()
+    {
+        return 'This is a looong phrase. This is a looong phrase. This is a looong phrase. This is a looong phrase. This is a looong phrase. This is a looong phrase. This is a looong phrase. ';
+    }
+
+    public function MyUglyMethodAgain()
+    {
+        return 'This is a looong phrase. This is a looong phrase. This is a looong phrase. This is a looong phrase. This is a looong phrase. This is a looong phrase. This is a looong phrase. ';
+    }
+
+
+    public function MyUglyMethodAgainAgain()
+    {
+        return 'This is a looong phrase. This is a looong phrase. This is a looong phrase. This is a looong phrase. This is a looong phrase. This is a looong phrase. This is a looong phrase. ';
+    }
+
+
+}
+
+
+
